ba98eb110125e5f5f8199ae172ce78114fb52882
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-29 19:05:29 +0000

    update front page & begin hw2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ st.markdown('# 488 Homework')
 st.markdown('## :red[Ava Langanki]')
 
 p1 = st.Page('homework/hw1.py', title='Homework 1 - Document QA', icon='👩‍💻', default=False)
+p2 = st.Page('homework/hw2.py', title='Homework 2 - URL Summarizer with Multiple LLMs', icon='💡', default=True)
 
-pg = st.navigation([p1])
+pg = st.navigation([p1, p2])
 
 pg.run()
